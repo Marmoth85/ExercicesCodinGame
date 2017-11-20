ee474460cecb4d42637af6e8af6927149f24bca0
Marmoth85 <projets.stats.info@gmail.com>
2017-11-20 18:03:53 +0100

    Fixed wrong identation

--- easy/C++/the_descent_1.cpp
+++ easy/C++/the_descent_1.cpp
@@ -8,8 +8,8 @@ using namespace std;
 int main()
 {
     while (1) {
-		int max_height = -1;
-		int mountain_index = -1;
+        int max_height = -1;
+        int mountain_index = -1;
         for (int i = 0; i < 8; i++) {
             int mountainH;
             cin >> mountainH; cin.ignore();
