9168eaa2567d0fdbbbcaffe91a02bf4876078b40
Marmoth85 <projets.stats.info@gmail.com>
2017-11-20 17:55:52 +0100

    Added easy/c++ The Descent exercice

---
+++ easy/C++/the_descent_1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    while (1) {
+		int max_height = -1;
+		int mountain_index = -1;
+        for (int i = 0; i < 8; i++) {
+            int mountainH;
+            cin >> mountainH; cin.ignore();
+			if (mountainH > max_height) {
+				max_height = mountainH;
+				mountain_index = i;
+			}
+        }
+        cout << mountain_index << endl;
+    }
+}
