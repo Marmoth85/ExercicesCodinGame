f4886063f77117d8829242ccc0f23b5b3e482181
Marmoth85 <projets.stats.info@gmail.com>
2017-12-15 13:55:08 +0100

    Fixed Medium / C++ Bender - Episode 1 PUZZLE

--- medium/C++/bender_1.cpp
+++ medium/C++/bender_1.cpp
@@ -10,14 +10,8 @@ using namespace std;
 vector<int> findPosition(int next_i, int next_j, const vector<vector<char>> & grid, const char & caracter) {
     vector<int> result;
     bool found = false;
-    cerr << "Forbidden coords : " << next_i << "    " << next_j << endl;
-    cerr << "Char searched : " << caracter << endl;
-    cerr << "grid.size() = " << grid.size() << endl;
     for (int i = 0; i < grid.size() && !found; ++i) {
-        cerr << "grid[0].size() = " << grid[0].size() << endl;
         for (int j = 0; j < grid[0].size() && !found; ++j) {
-            cerr << grid[i][j];
-            if (grid[i][j] == caracter) cerr << endl << "trouvé en " << i << ", " << j << endl;
             if (grid[i][j] == caracter) {
                 if (next_i != i || next_j != j) {
                     found = true;
@@ -26,7 +20,6 @@ vector<int> findPosition(int next_i, int next_j, const vector<vector<char>> & gr
                 }
             }
         }
-        cerr << endl;
     }
     return (result);
 }
@@ -48,7 +41,6 @@ int main()
 	string next_direction = "SOUTH";
 	vector<string> moves;
 
-    cerr << "La grille est la suivante : " << endl;
     for (int i = 0; i < L; i++) {
         string row;
         getline(cin, row);
@@ -56,7 +48,6 @@ int main()
 			char cell = row[j];
 			grid[i][j] = cell;
 		}
-		cerr << row << endl;
     }
 	
 	int counter = 0;
@@ -64,21 +55,15 @@ int main()
 	int test_direction = 1;
 	posBender = findPosition(-1, -1, grid, '@');
 	
-	cerr << "On entre dans le while" << endl;
 	while(!died && counter < 10000) {
 	    nextPosBender[0] = posBender[0];
 	    nextPosBender[1] = posBender[1];
-	    cerr << "Bender est en (x, y) = (" << posBender[0] << ", " << posBender[1] << ")" << endl;
-	    cerr << "Direction : " << direction << endl;
-	    cerr << "Next direction : " << next_direction << endl;
-		if (next_direction == "SOUTH") nextPosBender[0] = posBender[0] + 1;
-		if (next_direction == "NORTH") nextPosBender[0] = posBender[0] - 1;
-		if (next_direction == "WEST") nextPosBender[1] = posBender[1] - 1;
-		if (next_direction == "EAST") nextPosBender[1] = posBender[1] + 1;
-		cerr << "Les coordonnées suivantes sont : (" << nextPosBender[0] << ", " << nextPosBender[1] << ")" << endl;
+		if (direction == "SOUTH") nextPosBender[0] = posBender[0] + 1;
+		if (direction == "NORTH") nextPosBender[0] = posBender[0] - 1;
+		if (direction == "WEST") nextPosBender[1] = posBender[1] - 1;
+		if (direction == "EAST") nextPosBender[1] = posBender[1] + 1;
 		char next_cell = grid[nextPosBender[0]][nextPosBender[1]];
 		string act = action[next_cell];
-		cerr << "Action is " << act << endl;
 		if (act == "OBSTACLE" && breaker) {
 			grid[nextPosBender[0]][nextPosBender[1]] = ' ';
 			act = "WALK";
@@ -93,21 +78,14 @@ int main()
 		if (act == "TRANSLATE") {
 		    vector<int> temp;
 		    temp = findPosition(nextPosBender[0], nextPosBender[1], grid, 'T');
-		    cerr << "truc" << endl;
-		    cerr << nextPosBender.size() << endl;
-		    cerr << temp.size() << endl;
 		    nextPosBender = temp;
-		    cerr << nextPosBender.size() << endl;
-		    cerr << "Les coordonnées suivantes sont : (" << nextPosBender[0] << ", " << nextPosBender[1] << ")" << endl;
 		    act = "WALK";
 		}
 		if (act == "START") act == "WALK";
 		if (act == "INVERT") {
 		    map<string, int>::iterator it = orientation.begin();
 		    while (it != orientation.end()) {
-		        cerr << "Priority is : " << it -> second << endl;
 		        it -> second = 5 - it -> second; // 1->4, 2->3, 3->2, 4->1.
-		        cerr << "Prioriry now is : " << it -> second << endl;
 		        ++it;
 		    }
 		    act = "WALK";
@@ -131,10 +109,11 @@ int main()
 		if (act == "DEVIATE") {
 		    map<string, int>::iterator it;
 		    for (it = orientation.begin(); it != orientation.end(); ++it) {
-		        if (it -> second == test_direction) next_direction = it -> first;
+		        if (it -> second == test_direction) direction = it -> first;
 		    }
 		    ++test_direction;
 		    if (test_direction > 4) test_direction = 1;
+		    next_direction = direction;
 		    continue;
 		}
 		if (act == "DIE") {
@@ -144,8 +123,8 @@ int main()
 		if (act == "WALK") {
 		    posBender[0] = nextPosBender[0];
 		    posBender[1] = nextPosBender[1];
-		    direction = next_direction;
 		    moves.push_back(direction);
+		    direction = next_direction;
 		    test_direction = 1;
 		    ++counter;
 		}
